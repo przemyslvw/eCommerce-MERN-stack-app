7d9404b3592cab52b8f5c113a92d15ddf5d529f1
przemyslvw <przemyslvw@gmail.com>
2020-02-25 19:50:43 +0100

    schema User

---
+++ modules/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.Schema({
+  username: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+});
+
+modules.exports = mongoose.model("user", User);
