a3e331e1094938447fef480e8f05f32b192506c4
przemyslvw <przemyslvw@gmail.com>
2020-02-25 20:24:17 +0100

    register User in mongo

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const User = require("../modules/User");
 const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
@@ -17,12 +19,37 @@ router.post(
       .isEmpty()
   ],
   async (req, res) => {
-    const { username, email, password } = req.body;
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    // obsługa błędów
+    try {
+      const { username, email, password } = req.body;
+      let user = await User.findOne({ email });
+      const errors = validationResult(req);
+      // warunek jeśli dany email istnieje
+      if (user) {
+        return res.status(401).json({ msg: "User already exist" });
+      }
+      // warunek jeśli pola nie wypełnione
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      // tworzenie nowego użytkownika
+      user = new User({
+        email,
+        password,
+        username
+      });
+      // szyfrowanie hasła
+      let salt = await bcryptjs.genSalt(5);
+      user.password = await bcryptjs.hash(password, salt);
+
+      // zapis  do bazy
+      await user.save();
+
+      res.send("send");
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ msg: "Server error ..." });
     }
-    res.send("send");
   }
 );
 
