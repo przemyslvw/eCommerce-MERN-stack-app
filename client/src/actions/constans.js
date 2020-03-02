97091bd76d5bb18f5b58f4b5e329cda6c57a2f6c
przemyslvw <przemyslvw@gmail.com>
2020-03-02 23:36:54 +0100

    login and error action

--- client/src/actions/constans.js
+++ client/src/actions/constans.js
@@ -1,1 +1,3 @@
-export const CREATE_USER = "CREATE_USER";
+export const USER_CREATE = "USER_CREATE";
+export const USER_ERROR = "USER_ERROR";
+export const USER_LOGIN = "USER_LOGIN";
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,21 +1,31 @@
-import { CREATE_USER } from "../actions/constans";
+import { USER_CREATE, USER_ERROR, USER_LOGIN } from "../actions/constans";
 
 const initialState = {
   token: localStorage.getItem("token"),
   isLoading: false,
   isAuthenticated: false,
-  user: null
+  user: null,
+  error: null
 };
 
 const auth = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
-    case CREATE_USER:
+    case USER_CREATE:
+    case USER_LOGIN:
       localStorage.setItem("token", payload.token);
       return {
         ...state,
         isLoading: true,
         isAuthenticated: true
+      };
+    case USER_ERROR:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        isLoading: true,
+        isAuthenticated: false,
+        error: payload
       };
     default:
       return state;
