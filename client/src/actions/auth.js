487fb913621929a5593d47ea42e8fd4346888191
przemyslvw <przemyslvw@gmail.com>
2020-03-05 14:05:06 +0100

    action register

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN, USER_ERROR } from "./constans";
+import { USER_LOGIN, USER_ERROR, USER_CREATE } from "./constans";
 import axios from "axios";
 
 export const login = (password, email) => async dispatch => {
@@ -16,6 +16,29 @@ export const login = (password, email) => async dispatch => {
     );
     dispatch({
       type: USER_LOGIN,
+      payload: response.data
+    });
+    dispatch(loadUser());
+  } catch (error) {
+    dispatch({ type: USER_ERROR, payload: error });
+  }
+};
+
+export const registerUser = (username, password, email) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const body = JSON.stringify({ username, password, email });
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      body,
+      config
+    );
+    dispatch({
+      type: USER_CREATE,
       payload: response.data
     });
     dispatch(loadUser());
