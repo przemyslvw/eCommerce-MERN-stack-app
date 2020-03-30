925ea4ad7cb7ba463d5af9845f3c37dc045df2e1
unknown <przemyslvw@gmail.com>
2020-03-30 13:16:13 +0200

    login page functions

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { login } from '../actions/auth';
 
-const Login = () => {
-  return (
-    <div className="page-wrapper">
-      <header>Login</header>
-      <div className="mt-10 inputs-wrapper">
-        <input
-          type="text"
-          name="firstName"
-          placeholder="E-mail"
-          onfocus="this.placeholder = ''"
-          onblur="this.placeholder = 'First Name'"
-          required
-          className="single-input"
-        />
-        <input
-          type="password"
-          name="pass"
-          placeholder="Password"
-          onfocus="this.placeholder = ''"
-          onblur="this.placeholder = 'First Name'"
-          required
-          className="single-input"
-        />
-      </div>
-      <div>
-        <a href="#" className="genric-btn success">
-          Submit
-        </a>
-      </div>
-    </div>
-  );
-};
+const Login = ({ login}) => {
+    const [ data,setData ] = useState({
+        email: '',
+        password: ''
+    });
 
-export default Login;
+    const { email,password } = data;
+
+    const onChange = e => {
+        setData({...data, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = () => {
+        login(password,email);
+    }
+
+    return (
+        <div className="page-wrapper">
+            <header>
+            Login
+            </header>
+	        <div className="mt-10 inputs-wrapper">
+				<input type="text" name="email" placeholder="E-mail" value={ email } onChange={(e) => onChange(e)}
+				required className="single-input"/>
+                <input type="password" name="password" placeholder="Password" value={ password } onChange={(e) => onChange(e)}
+				required className="single-input"/>
+			</div>
+            <div onClick={() => onSubmit()}>
+                <a href="#" className="genric-btn success">Submit</a>
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { login })(Login);
