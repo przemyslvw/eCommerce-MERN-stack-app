b9bdbb1ff13f6d046f597391f1b8fbad312528a9
unknown <przemyslvw@gmail.com>
2020-03-30 14:56:19 +0200

    setAuthToken fix

--- client/src/auth/setAuthToken.js
+++ client/src/auth/setAuthToken.js
@@ -1,8 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
-const setAutchToken = token => {
-  if (token) axios.default.headers.common["authentication-token"] = token;
-  else delete axios.defaults.headers.common["authentication-token"];
-};
+//Sprawdzanie czy użytkownik posiada token
+const setAuthToken = token => {
+    //Przypisanie tokenu
+    if(token) axios.defaults.headers.common['authentication-token'] = token;
+    //Usunięcie tokenu
+    else delete axios.defaults.headers.common['authentication-token'];
+}
 
-export default setAutchToken;
+export default setAuthToken;
